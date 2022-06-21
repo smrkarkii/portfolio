30f13a592384951bf7fc7b7c9056c4de4c0b85b5
smrkarkii <076bct080.smriti@pcampus.edu.np>
2022-06-21 21:29:20 +0545

    Bio

---
+++ html-site/index.js
@@ -0,0 +1,12 @@
+
+// $(document).ready({
+//     $(window).scroll(function() 
+//     {
+//         if($(window).scrollTop()){
+//             $("nav").addClass("sticky navbarOffsetMargin");
+//         }
+//         else{
+//             $('nav').removeClass("sticky navbarOffsetMargin");
+//         }
+//     })
+// })
