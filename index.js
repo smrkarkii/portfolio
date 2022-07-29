43f35bb4aee7dc56070265fe64e1bd5a57c7cc8d
smrkarkii <076bct080.smriti@pcampus.edu.np>
2022-07-29 18:18:34 +0545

    contact links fixed but error i nscrolling

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-
-
-    // $("#cv-download").on('click', function () {
-        
-    //     attr({target: '_blank', 
-    // href  : 'file/cv_smriti_karki.pdf'});
-    // })
+// // document.addEventListener("DOMContentLoaded", function(){
+// // window.addEventListener("scroll" ,function (){
+// //     if(window.scrollY>50) {
+// //         document.getElementById("navbar-top").classList.add("fixed-nav");
+// //         navbar_height = document.querySelector('.navbar').offsetHeight;
+// //         this.document.body.style.marginTop = navbar_height;
+// //     }
+// //     else{
+// //         this.document.body.style.marginTop = '0'
+// //         document.getElementById("navbar-top").classList.remove("fixed-nav");
+// //     }
+  
+// // })
+// // })
+// document.addEventListener("DOMContentLoaded", function(){
+//     window.addEventListener('scroll', function() {
+//         if (window.scrollY > 50) {
+//           document.getElementById('navbar-top').classList.add('fixed-nav');
+//           // add padding top to show content behind navbar
+//           navbar_height = document.querySelector('.navbar').offsetHeight;
+//           document.body.style.paddingTop = navbar_height + 'px';
+//         } else {
+//           document.getElementById('navbar_top').classList.remove('fixed-nav');
+//            // remove padding top from body
+//           document.body.style.paddingTop = '0';
+//         } 
+//     });
+//   }); 
