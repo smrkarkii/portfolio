1252b0f62f2f9f2e6a0143f03480941368ed53a6
smrkarkii <076bct080.smriti@pcampus.edu.np>
2022-08-07 22:44:01 +0545

    issues solved except instagram link, and width in mobile section

--- index.js
+++ index.js
@@ -27,16 +27,16 @@
 // //         } 
 // //     });
 // //   }); 
-// $(document).ready(function () {
-//   $(window).scroll(function(){
-//     if ($(this).scrollTop() > 50) {
-//        $('#navbar-top').addClass('fixed-nav');
-//       //  navbar_height = document.querySelector('.navbar').offsetHeight;
-//       //  $(".second-content").css("padding-top",'300px')
-//     } else {
-//        $('#navbar-top').removeClass('fixed-nav');
-//     }
-//   });
+$(document).ready(function () {
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 0) {
+       $('#navbar-top').addClass('fixed-nav');
+      //  navbar_height = document.querySelector('.navbar').offsetHeight;
+      //  $(".second-content").css("padding-top",'300px')
+    } else {
+       $('#navbar-top').removeClass('fixed-nav');
+    }
+  });
 
-// })
+})
 
