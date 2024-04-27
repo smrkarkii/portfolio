ab2b1011bc73922051b029a1502c1dcdfcb219ec
smrkarkii <076bct080.smriti@pcampus.edu.np>
2024-04-27 20:41:49 +0545

    skill section redesigned

---
+++ src/Data/Skill.js
@@ -0,0 +1,98 @@
+import solidity from "../images/icons/solidity.svg";
+import cpp from "../images/icons/c.svg";
+import hardhat from "../images/icons/hardhat.svg";
+import postman from "../images/icons/postman.svg";
+import git from "../images/icons/git.svg";
+import vs from "../images/icons/vs.svg";
+
+export const skillsDataLanguages = [
+  {
+    title: "HTML",
+    level: "80%",
+    qual: "Experienced",
+    icon: "faHtml5",
+    color: "orangered",
+  },
+
+  {
+    title: "CSS",
+    level: "80%",
+    qual: "Experienced",
+    color: "#264DE4",
+    icon: "faCss3Alt",
+  },
+
+  {
+    title: "JS",
+    level: "60%",
+    qual: "Intermediate",
+    color: "yellow",
+    bgcol: "black",
+    bg: "black",
+    icon: "faJs",
+  },
+  {
+    title: "C/C++",
+    level: "60%",
+    qual: "Intermediate",
+    color: "green",
+    image: cpp,
+    size: "95%",
+  },
+  {
+    title: "Solidity",
+    level: "60%",
+
+    bg: "white",
+    size: "65%",
+    image: solidity,
+  },
+];
+export const skillsDataFrameworks = [
+  {
+    title: "Node JS",
+    level: "60%",
+    qual: "Intermediate",
+    color: "green",
+    icon: "faNodeJs",
+  },
+  {
+    title: "React JS",
+    level: "80%",
+    qual: "Experienced",
+    icon: "faReact",
+    color: "#01d9fc",
+  },
+  // {
+  //   title: "Express",
+  //   level: "60%",
+  //   qual: "Intermediate",
+  //   color: "green",
+  //   icon: "faNodeJs",
+  // },
+  {
+    title: "Hardhat",
+
+    image: hardhat,
+  },
+];
+export const skillsDataDevTools = [
+  {
+    title: "Github",
+    image: git,
+    color: "black",
+  },
+  {
+    title: "VS Code",
+
+    image: vs,
+  },
+  {
+    title: "Postman",
+    level: "80%",
+    qual: "Experienced",
+    color: "#264DE4",
+
+    image: postman,
+  },
+];
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,39 +5,148 @@ import {
   faCss3Alt,
   faJs,
   faNodeJs,
+  faGithub,
 } from "@fortawesome/free-brands-svg-icons";
+
+import {
+  skillsDataDevTools,
+  skillsDataFrameworks,
+  skillsDataLanguages,
+} from "../Data/Skill";
 import "../css/skills.css";
+import "../Data/Skill";
+
 export default function Skills(props) {
-  const arr = [faHtml5, faReact, faCss3Alt, faJs, faNodeJs];
-  let skillsData = props.data;
+  const arr = [faHtml5, faCss3Alt, faJs];
+  const arr1 = [faNodeJs, faReact];
+  const arr2 = [faGithub, faReact];
+
   return (
     <section id="skills">
       <div className="gridSkill">
         <h2 id="skill">
           <b>SKILLS</b>
         </h2>
-        {/* <div className="skill-illustrator">
-          {skillsData.skills.map((skill) => (
+        <h6 className="skill-type" style={{ color: "white" }}>
+          Languages
+        </h6>
+        <hr
+          style={{
+            color: "white",
+          }}
+        />
+
+        <div className="skills-wrapper skills-languages">
+          {skillsDataLanguages.map((skill, i) => (
             <>
-              <h3>{skill.title}</h3>
+              {console.log(skill.image)}
               <div
-                className="illustrator-bar bar skill-bar"
-                style={{ width: skill.level, height: "20px" }}
+                className="indiv-skill"
+                style={{ backgroundColor: skill.bg }}
               >
-                <p>{skill.qual}</p>
+                {/* <img src={logo} className="skill-logo" alt="" /> */}
+
+                {skill.icon ? (
+                  <FontAwesomeIcon
+                    style={{ color: skill.color, backgroundColor: skill.bgcol }}
+                    className="logo-icon"
+                    icon={arr[i]}
+                  />
+                ) : (
+                  <img
+                    style={{
+                      width: skill.size,
+
+                      marginTop: "8px",
+                    }}
+                    src={skill.image}
+                    alt="no file"
+                  />
+                )}
+                <p style={{ textAlign: "center" }} className="skill-title">
+                  {skill.title}
+                </p>
               </div>
             </>
           ))}
-        </div> */}
-        <div className="skills-wrapper">
-          {skillsData.skills.map((skill, i) => (
+        </div>
+
+        <h6 className="skill-type" style={{ color: "white" }}>
+          Frameworks
+        </h6>
+        <hr
+          style={{
+            color: "white",
+          }}
+        />
+        <div className="skills-wrapper skills-frameworks">
+          {skillsDataFrameworks.map((skill, i) => (
             <>
-              <div className="indiv-skill">
+              <div
+                className="indiv-skill"
+                style={{ backgroundColor: skill.bg }}
+              >
                 {/* <img src={logo} className="skill-logo" alt="" /> */}
-                <FontAwesomeIcon className="logo-icon" icon={arr[i]} />{" "}
-                {/* <FontAwesomeIcon className="logo-icon" icon={faReact} />{" "} */}
-                {/* <FontAwesomeIcon icon="fa-brands fa-react" /> */}
-                <p className="skill-title">{skill.title}</p>
+                {skill.icon ? (
+                  <FontAwesomeIcon
+                    style={{ color: skill.color }}
+                    className="logo-icon"
+                    icon={arr1[i]}
+                  />
+                ) : (
+                  <img
+                    src={skill.image}
+                    alt="no file"
+                    style={{
+                      width: "90%",
+
+                      marginTop: "30px",
+                    }}
+                  />
+                )}
+                <p style={{ textAlign: "center" }} className="skill-title">
+                  {skill.title}
+                </p>
+              </div>
+            </>
+          ))}
+        </div>
+        <h6 className="skill-type" style={{ color: "white" }}>
+          Developer Tools
+        </h6>
+        <hr
+          style={{
+            color: "white",
+          }}
+        />
+        <div className="skills-wrapper skills-dev-tools">
+          {skillsDataDevTools.map((skill, i) => (
+            <>
+              <div
+                className="indiv-skill"
+                style={{ backgroundColor: skill.bg }}
+              >
+                {/* <img src={logo} className="skill-logo" alt="" /> */}
+                {skill.icon ? (
+                  <FontAwesomeIcon
+                    style={{ color: skill.color }}
+                    className="logo-icon"
+                    icon={arr2[i]}
+                  />
+                ) : (
+                  <img
+                    src={skill.image}
+                    alt="no file"
+                    style={{
+                      width: "90%",
+                      height: "1 px",
+                      marginTop: "8px",
+                    }}
+                  />
+                )}
+                <p style={{ textAlign: "center" }} className="skill-title">
+                  {skill.title}
+                </p>
               </div>
             </>
           ))}
